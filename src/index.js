876713f8ffc95b58148e3fbda0c8d62df2094b65
MandasK <king.amanda.r@gmail.com>
2020-07-18 11:11:35 -0500

    adding in bootstrap

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import Nutshell from './components/nutshell';
 import * as serviceWorker from './serviceWorker';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 ReactDOM.render(
   <Router>
