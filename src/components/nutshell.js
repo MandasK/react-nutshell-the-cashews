1803e3288a558faa025a8e1c76d93456af94d481
Tyler Hilliard <tylerghilliard@gmail.com>
2020-07-20 19:08:07 -0500

    added friends search and add functionality

--- src/components/nutshell.js
+++ src/components/nutshell.js
@@ -18,7 +18,7 @@ const Nutshell = (props) => {
     const [hasUser, setHasUser] = useState(isAuthenticated());
     const setUser = (user) => {
       sessionStorage.setItem("activeUserID", JSON.stringify(user.userId));
-      sessionStorage.setItem("activeUser", JSON.stringify(user.userName))
+      sessionStorage.setItem("activeUser", user.userName)
       setHasUser(isAuthenticated())
       console.log(hasUser)
     }
